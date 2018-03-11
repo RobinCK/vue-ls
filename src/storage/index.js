994ad0ee7458a5cc039f1d616f61424a088e761f
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-03-11 15:13:17 +0200

    Removed the cookie storage interface

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,10 +1,8 @@
-import { CookieStorageInterface } from './CookieStorageInterface';
 import { MemoryStorageInterface } from './MemoryStorageInterface';
 import { WebStorage } from './WebStorage';
 import { WebStorageEvent } from './WebStorageEvent';
 
 export {
-  CookieStorageInterface,
   MemoryStorageInterface,
   WebStorage,
   WebStorageEvent,
