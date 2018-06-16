6600a55d64c89235b0f0e4e151c1b364e902390d
Igor Ognichenko <ognichenko.igor@gmail.com>
2018-06-16 19:25:55 +0300

    refactoring after update uglify

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import {uglify} from 'rollup-plugin-uglify';
 import localResolve from 'rollup-plugin-local-resolve';
 
 export default {
